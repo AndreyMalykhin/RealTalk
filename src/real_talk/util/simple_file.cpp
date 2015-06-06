52bf2b744760a97e45965df7462821aea6d72a9e
AndreyMalykhin <shell100500@gmail.com>
2015-06-06 17:44:00 +0300

    Compiler

--- src/real_talk/util/simple_file.cpp
+++ src/real_talk/util/simple_file.cpp
@@ -1,27 +1,65 @@
 
 #include <boost/filesystem/fstream.hpp>
+#include <boost/format.hpp>
+#include <limits>
+#include <sstream>
+#include <string>
+#include "real_talk/code/code.h"
+#include "real_talk/util/errors.h"
 #include "real_talk/util/simple_file.h"
 
+using std::string;
 using std::ios;
 using std::istream;
+using std::istringstream;
+using std::streampos;
+using std::streamsize;
 using std::unique_ptr;
+using std::numeric_limits;
 using boost::filesystem::path;
 using boost::filesystem::ifstream;
+using boost::filesystem::ofstream;
+using boost::format;
 using real_talk::code::Code;
 
 namespace real_talk {
 namespace util {
 
 void SimpleFile::Write(const path &file_path, const Code &code) const {
-  assert(false);
+  try {
+    ofstream stream;
+    stream.exceptions(ios::failbit | ios::badbit);
+    stream.open(file_path, ios::binary | ios::trunc);
+
+    if (code.GetSize() > numeric_limits<streamsize>::max()) {
+      const string msg = (format("Code size exceeds max writable size; max=%1%")
+                          % numeric_limits<streamsize>::max()).str();
+      throw IOError(msg);
+    }
+
+    stream.write(reinterpret_cast<const char*>(code.GetData()), code.GetSize());
+  } catch (const ios::failure &e) {
+    throw IOError(e.what());
+  }
 }
 
 unique_ptr<istream> SimpleFile::Read(const path &file_path) const {
-  auto *fstream = new ifstream();
-  unique_ptr<istream> stream(fstream);
-  fstream->exceptions(ios::failbit | ios::badbit);
-  fstream->open(file_path, ios::binary);
-  return stream;
+  try {
+    ifstream file_stream;
+    file_stream.exceptions(ios::failbit | ios::badbit);
+    file_stream.open(file_path, ios::binary);
+    file_stream.seekg(0, ios::end);
+    streampos content_size = file_stream.tellg();
+    assert(content_size >= 0);
+    file_stream.seekg(0, ios::beg);
+    string content(static_cast<size_t>(content_size), ' ');
+    file_stream.read(&content[0], content_size);
+    unique_ptr<istream> string_stream(new istringstream(content));
+    string_stream->exceptions(ios::failbit | ios::badbit);
+    return string_stream;
+  } catch (const ios::failure &e) {
+    throw IOError(e.what());
+  }
 }
 }
 }
--- test/real_talk/util/simple_file_test.cpp
+++ test/real_talk/util/simple_file_test.cpp
@@ -1,16 +1,26 @@
 
+#include <boost/numeric/conversion/cast.hpp>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <vector>
+#include "real_talk/code/code.h"
 #include "real_talk/test/test_config.h"
 #include "real_talk/util/simple_file.h"
 
+using std::vector;
 using std::streampos;
+using std::streamsize;
 using std::unique_ptr;
 using std::istream;
 using std::ios;
 using testing::Test;
+using boost::numeric_cast;
 using boost::filesystem::path;
+using boost::filesystem::file_size;
+using boost::filesystem::exists;
+using boost::filesystem::remove;
 using real_talk::test::TestConfig;
+using real_talk::code::Code;
 
 namespace real_talk {
 namespace util {
@@ -18,25 +28,61 @@ namespace util {
 class SimpleFileTest: public Test {
  protected:
   virtual void SetUp() override {}
-  virtual void TearDown() override {}
+
+  virtual void TearDown() override {
+    while (!files_to_remove_.empty()) {
+      remove(files_to_remove_.back());
+      files_to_remove_.pop_back();
+    }
+  }
+
+  void AssertFileContent(const char *expected_content,
+                         size_t expected_size,
+                         istream *stream,
+                         const path &file_path) {
+    uintmax_t actual_size = file_size(file_path);
+    ASSERT_EQ(expected_size, actual_size);
+    unique_ptr<char[]> actual_content(new char[actual_size]);
+    stream->read(
+        actual_content.get(), numeric_cast<streamsize>(actual_size));
+    ASSERT_EQ(0, memcmp(expected_content, actual_content.get(), expected_size));
+  }
+
+  vector<path> files_to_remove_;
 };
 
 TEST_F(SimpleFileTest, Read) {
   path file_path(TestConfig::GetResourceDir() / path("file.txt"));
   char expected_content[] = "abc\n";
-  streampos expected_content_size = 4;
+  size_t expected_content_size = 4;
   streampos expected_stream_position = 0;
   unique_ptr<istream> stream = SimpleFile().Read(file_path);
   ios::iostate expected_exceptions_mask = ios::failbit | ios::badbit;
   ASSERT_EQ(expected_exceptions_mask, stream->exceptions());
   ASSERT_EQ(expected_stream_position, stream->tellg());
-  stream->seekg(0, ios::end);
-  streampos actual_content_size = stream->tellg();
-  stream->seekg(0, ios::beg);
-  ASSERT_EQ(expected_content_size, actual_content_size);
-  unique_ptr<char[]> actual_content(new char[actual_content_size]);
-  stream->read(actual_content.get(), actual_content_size);
-  ASSERT_STREQ(expected_content, actual_content.get());
+  AssertFileContent(
+      expected_content, expected_content_size, stream.get(), file_path);
+}
+
+TEST_F(SimpleFileTest, Write) {
+  path file_path(TestConfig::GetResourceDir() / path("file2.txt"));
+  files_to_remove_.push_back(file_path);
+  Code code;
+  code.WriteChar('a');
+  code.WriteChar('b');
+  size_t expected_content_size = 2;
+  char expected_content[] = "ab";
+  SimpleFile file;
+  ASSERT_TRUE(!exists(file_path));
+  file.Write(file_path, code);
+  unique_ptr<istream> stream = file.Read(file_path);
+  AssertFileContent(
+      expected_content, expected_content_size, stream.get(), file_path);
+
+  file.Write(file_path, code);
+  stream = file.Read(file_path);
+  AssertFileContent(
+      expected_content, expected_content_size, stream.get(), file_path);
 }
 }
 }
